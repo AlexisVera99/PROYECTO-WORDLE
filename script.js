6f5d758a3b65b9f9b83b7a138e614b00bfd736f7
AlexisVera99 <151799902+AlexisVera99@users.noreply.github.com>
2023-12-01 18:22:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ function obtenerPalabra(diccionario){
     let max = diccionario.length - 1;
     let min = 0;
     let i = Math.floor(Math.random() *(max - min + 1)) + min;
-    palabra = diccionario[i];
-    return;
+    palabraElegida = diccionario[i];
+    return palabraElegida;
 }
-
 function intentar(){
     const INTENTO = leerIntento();
     if(INTENTO === palabra){
@@ -22,17 +21,16 @@ function intentar(){
     const GRID = document.getElementById("grid");
     const ROW = document.createElement("div");
     ROW.className = "row";
-
     for (let i in INTENTO){
     const SPAN = document.createElement("span");
     SPAN.className = "letter"; 
     SPAN.innerHTML = INTENTO[i];
     let color;
     let letra = INTENTO[i];
-    if (letra == palabra[i]){
+    if (letra == palabra [i]) {
         color = "#79b851";
     } else if (palabra.includes(letra)){
-        color = "#f3c237";
+        color = "yellow";
     } else {
         color = "#a4aec4";
     }
@@ -43,9 +41,15 @@ intentos--;
     if (intentos == 0){
     terminar ("<h1>PERDISTE</h1>");
 }
-    GRID.appendChild(ROW)
+    GRID.appendChild(ROW);
 
 }
+function leerIntento(){
+    const INPUT = document.getElementById("guess-input");
+    let intento = INPUT.value;
+    return intento.toUpperCase(); 
+}
+    
 function terminar(mensaje){
     let input = document.getElementById("guess-input");
     input.disabled = true;
